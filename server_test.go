74569b68c4d0826844b1a20fafe1d53dd858c4d9
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-09-08 17:04:29 +0500

    refactor: перенес StubPlayerStore в тест файл

--- server_test.go
+++ server_test.go
@@ -7,6 +7,20 @@ import (
 	"testing"
 )
 
+type StubPlayerStore struct {
+	scores   map[string]int
+	winCalls []string
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+func (s *StubPlayerStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
+}
+
 func TestGETPlayers(t *testing.T) {
 	store := StubPlayerStore{
 		map[string]int{

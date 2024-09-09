ddd3ac71980b6e69d7bf21fd993b246f7a5e860f
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-09-09 23:09:15 +0500

    refactor: отдельная инициализация для БД файла

--- file_system_store.go
+++ file_system_store.go
@@ -14,17 +14,10 @@ type FileSystemPlayerStore struct {
 
 func NewFileSystemStore(file *os.File) (*FileSystemPlayerStore, error) {
 
-	file.Seek(0, io.SeekStart)
-
-	info, err := file.Stat()
+	err := initialisePlayerDBFile(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte("[]"))
-		file.Seek(0, io.SeekStart)
+		return nil, fmt.Errorf("problem initialising player db file %v", err)
 	}
 
 	league, err := NewLeague(file)
@@ -37,6 +30,23 @@ func NewFileSystemStore(file *os.File) (*FileSystemPlayerStore, error) {
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
+}
+
+func initialisePlayerDBFile(file *os.File) error {
+	file.Seek(0, io.SeekStart)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, io.SeekStart)
+	}
+
+	return nil
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {

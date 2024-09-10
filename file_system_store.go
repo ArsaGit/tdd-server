93eb8dec1474d46992dc442cd62b13334151d310
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-09-10 22:50:50 +0500

    refactor: вынес открытие файла в свою функцию

--- file_system_store.go
+++ file_system_store.go
@@ -33,6 +33,26 @@ func NewFileSystemStore(file *os.File) (*FileSystemPlayerStore, error) {
 	}, nil
 }
 
+func FileSystemPlayerStoreFromFile(path string) (*FileSystemPlayerStore, func(), error) {
+	db, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem opening %s %v", path, err)
+	}
+
+	closeFunc := func() {
+		db.Close()
+	}
+
+	store, err := NewFileSystemStore(db)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem creating file system player store, %v", err)
+	}
+
+	return store, closeFunc, nil
+}
+
 func initialisePlayerDBFile(file *os.File) error {
 	file.Seek(0, io.SeekStart)
 

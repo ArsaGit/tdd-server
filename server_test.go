855b4b03885be82631e4de8d6e3a1045a62ed443
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-09-12 14:31:25 +0500

    feat: новый endpoint и обработчик

--- server_test.go
+++ server_test.go
@@ -6,6 +6,19 @@ import (
 	"testing"
 )
 
+func TestGame(t *testing.T) {
+	t.Run("GET /game returns 200", func(t *testing.T) {
+		server := NewPlayerServer(&StubPlayerStore{})
+
+		request, _ := http.NewRequest(http.MethodGet, "/game", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		AssertStatus(t, response.Code, http.StatusOK)
+	})
+}
+
 func TestGETPlayers(t *testing.T) {
 	store := StubPlayerStore{
 		scores: map[string]int{
